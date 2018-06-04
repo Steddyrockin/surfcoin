c4ec51260af58ad3f8dc37f3ca46e1539107ffbc
Tomasz Zacharczuk <tomasz.zacharczuk@gmail.com>
2018-06-04 16:05:50 -0700

    Ownable tests

--- test/Surfcoin.test.js
+++ test/Surfcoin.test.js
@@ -16,6 +16,40 @@ require('chai')
 const Surfcoin = artifacts.require('Surfcoin');
 
 
+contract('Ownable', function (accounts) {
+  let ownable;
+
+  beforeEach(async function () {
+    ownable = await Surfcoin.new(100);
+  });
+
+  it('should have an owner', async function () {
+    let owner = await ownable.owner();
+    assert.isTrue(owner !== 0);
+  });
+
+  it('changes owner after transfer', async function () {
+    let other = accounts[1];
+    await ownable.transferOwnership(other);
+    let owner = await ownable.owner();
+
+    assert.isTrue(owner === other);
+  });
+
+  it('should prevent non-owners from transfering', async function () {
+    const other = accounts[2];
+    const owner = await ownable.owner.call();
+    assert.isTrue(owner !== other);
+    await assertRevert(ownable.transferOwnership(other, { from: other }));
+  });
+
+  it('should guard ownership against stuck state', async function () {
+    let originalOwner = await ownable.owner();
+    await assertRevert(ownable.transferOwnership(null, { from: originalOwner }));
+  });
+});
+
+
 contract('BaseToken', function ([_, owner, recipient, anotherAccount]) {
 
   beforeEach(async function () {
